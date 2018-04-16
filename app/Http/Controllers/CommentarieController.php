63ce062553512e11f90f24ecfab5ed9c9544a46a
ryfys <mrprizrak.vs@gmail.com>
2018-04-16 20:37:51 +0300

    create migration, model and controller

---
+++ app/Http/Controllers/CommentarieController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Commentarie;
+use Illuminate\Http\Request;
+
+class CommentarieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Commentarie  $commentarie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Commentarie $commentarie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Commentarie  $commentarie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Commentarie $commentarie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Commentarie  $commentarie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Commentarie $commentarie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Commentarie  $commentarie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Commentarie $commentarie)
+    {
+        //
+    }
+}
